2110096884e47b0b3490792cda06cf58b6bbb091
Son Hyeongmin <76472284+Hyeongmini@users.noreply.github.com>
2022-02-07 19:39:39 +0900

    docs: Delete 'for' add 'i' and print 'i'

--- test.py
+++ test.py
@@ -8,10 +8,6 @@ dp[0] = 0
 dp[1] = 1
 dp[2] = 2
     
-for i in range(3, n+1):
-    for j in range(1, i):
-        if i - j*j < 0:
-            break
-        dp[i] = min(dp[i], dp[i-j*j] + 1)
+i = 0
 
-print(dp[n])
+print(i)
