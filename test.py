dc9bced4088222a068ac5213b5a5e5e585559616
rlarjsgh <90206320+rlarjsgh@users.noreply.github.com>
2022-02-07 19:40:50 +0900

    style: change the name of list 'dp' to 'list'

--- test.py
+++ test.py
@@ -3,15 +3,15 @@
 
 n = int(input())
    
-dp = [1e12 for i in range(101010)]
-dp[0] = 0
-dp[1] = 1
-dp[2] = 2
+list = [1e12 for i in range(101010)]
+list[0] = 0
+list[1] = 1
+list[2] = 2
     
 for i in range(3, n+1):
     for j in range(1, i):
         if i - j*j < 0:
             break
-        dp[i] = min(dp[i], dp[i-j*j] + 1)
+        list[i] = min(list[i], list[i-j*j] + 1)
 
-print(dp[n])
+print(list[n])
